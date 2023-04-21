d43c362815534406c194f377a7b904b6f69adfab
Abdul Basit <abdbasit108@gmail.com>
2023-04-21 23:04:25 +0500

    Task 9 - Animations

--- to_do_app/lib/pages/splash_screen.dart
+++ to_do_app/lib/pages/splash_screen.dart
@@ -1,7 +1,8 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 import 'package:flutter/material.dart';
-import 'package:to_do_app/pages/home_page.dart';
+//import 'package:to_do_app/pages/home_page.dart';
+import 'package:to_do_app/pages/sign_up_page.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -19,7 +20,7 @@ class SplashScreen extends StatelessWidget {
         animationDuration: const Duration(milliseconds: 1500),
         splashIconSize: _height,
         splash: 'assets/splash.png',
-        nextScreen: HomePage(),
+        nextScreen: SignUpScreen(),
         // nextRoute: '/home_screen',
         // nextScreen: MainScreen(),
       ),
